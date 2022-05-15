519f9991e927e559aca1fe82ae756c530367ead3
Alfredo Gambino <alfredo.gambino@outlook.com>
2022-05-15 21:44:09 +0200

    video description

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,47 @@
+import { useState } from "react";
 import caretIcon from "../../assets/icons/caret.png";
 import { Button, ButtonType } from "../Button/Button";
 import styles from "./Header.module.scss";
 
 export const HeaderVideo = () => {
+  const [showDescr, setShowDescr] = useState<boolean>(false);
+
   return (
-    <header>
-      <div className={styles.headerTitle}>
-        <div className={styles.title}>Saying Hi to my customers</div>
-        <img src={caretIcon} alt="Hi" />
-      </div>
-      <div className={styles.headerTools}>
-        <Button type={ButtonType.fillGray} label="Cancel" />
-        <Button type={ButtonType.fillGreen} label="Save" />
-      </div>
-      
-    </header>
+    !showDescr ? (
+      <header>
+        <div className={styles.headerTitle} onClick={() => setShowDescr(true)}>
+          <div className={styles.title}>Saying Hi to my customers</div>
+          <img src={caretIcon} alt="Hi" />
+        </div>
+        <div className={styles.headerTools}>
+          <Button type={ButtonType.fillGray} label="Cancel" />
+          <Button type={ButtonType.fillGreen} label="Save" />
+        </div>
+      </header>
+      )
+      :
+      (
+        <div className={styles.headerOverlay}>
+          <div className={styles.headerOverlayContent}>
+            <div className={styles.headerTitle}>
+              <input name="title" value="Saying Hi to my customers" />
+              <textarea>Fusce quis magna vel ex pellentesque consequat sed et turpis. Vivamus bibendum rutrum euismod. Sed non sagittis est, semper</textarea>
+            </div>
+            <div className={styles.tags}>
+              <div className={styles.tag}>Email</div>
+              <div className={styles.tag}>Marketing</div>
+              <div className={styles.tag}>Greeting</div>
+              <div className={styles.tag}>Email</div>
+              <div className={styles.tag}>Marketing</div>
+              <div className={styles.tag}>Greeting</div>
+              </div>
+            <div className={styles.headerTools}>
+              <Button type={ButtonType.fillGreen} label="Save" onClick={() => setShowDescr(false)} />
+            </div>
+          </div>
+          <div className={styles.overlay}></div>
+        </div>
+      )
   )
 };
 
